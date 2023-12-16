652159e7e1b84cbefed1df6fb13429e33553126f
João Neto <jflorencioadv@gmail.com>
2023-12-16 15:23:35 +0100

    diversion-con-banderas

--- js/script.js
+++ js/script.js
@@ -1,35 +1,101 @@
 document.addEventListener('DOMContentLoaded', () => {
-    fetchCountries();
-});
-
-async function fetchCountries() {
+    getCountries()
+      .then(countries => displayCountries(countries))
+      .catch(error => console.error('Error fetching or displaying countries:', error));
+  });
+  
+  async function getCountries() {
     try {
-        const response = await fetch('https://restcountries.com/v3/all');
-        const countries = await response.json();
-        displayCountries(countries.sort((a, b) => a.name.common.localeCompare(b.name.common, 'en', { sensitivity: 'base' })));
+      const response = await fetch('https://restcountries.com/v3/all');
+      const countries = await response.json();
+      const sortedCountries = sortCountriesAlphabetically(countries);
+      return sortedCountries;
     } catch (error) {
-        console.error('Error fetching countries:', error);
+      throw error;
     }
-}
-
-function displayCountries(countries) {
-    const countriesList = document.getElementById('countries-list');
-    countriesList.innerHTML = ''; 
-
-    countries.forEach(country => {
-        const countryDiv = document.createElement('div');
-        countryDiv.className = 'country-item';
-        countryDiv.innerHTML = `
-            <img src="${country.flags.png}" alt="Flag of ${country.name.common}">
-            <p>${country.name.common}</p>
-        `;
-        countryDiv.addEventListener('click', () => showCountryDetails(country));
-        countriesList.appendChild(countryDiv);
+  }
+  
+  function sortCountriesAlphabetically(countries) {
+    return countries.sort((a, b) => {
+      const nameA = a.name.common.toUpperCase();
+      const nameB = b.name.common.toUpperCase();
+      return nameA.localeCompare(nameB);
     });
-}
-
-function showCountryDetails(country) {
-    
-    console.log('Mostrando detalles para:', country.name.common);
-    
-}
+  }
+  
+  // function displayCountries(countries) {
+  //   const countriesList = document.getElementById('countries-list');
+  //   countriesList.innerHTML = '';
+  
+  //   if (countries.length === 0) {
+  //     countriesList.innerHTML = 'No se encontraron países.';
+  //     return;
+  //   }
+  
+  //   // const countriesHTML = countries.map(country => `
+  //   //   <div class="country">
+  //   //     <img src="${country.flags[0]}" alt="${country.name.common} flag">
+  //   //     <h2>${country.name.common}</h2>
+  //   //   </div>
+  //   // `).join('');
+  
+  //   countriesList.innerHTML = countriesHTML;
+  
+  //   countries.forEach((country, index) => {
+  //     const countryElement = document.querySelectorAll('.country')[index];
+  //     countryElement.addEventListener('click', () => {
+  //       showCountryInfo(country);
+  //     });
+  //   });
+  // }
+  function displayCountries(countries) {
+    const countriesList = document.getElementById('countries-list');
+    countriesList.innerHTML = '';
+  
+    if (countries.length === 0) {
+      countriesList.innerHTML = 'No se encontraron países.';
+      return;
+    }
+  
+    countries.forEach(country => {
+      const countryHTML = `
+        <div class="country">
+          <img src="${country.flags[0]}" alt="${country.name.common} flag">
+          <h2>${country.name.common}</h2>
+        </div>
+      `;
+      
+      countriesList.innerHTML += countryHTML;
+    });
+  
+    countries.forEach((country, index) => {
+      const countryElement = document.querySelectorAll('.country')[index];
+      countryElement.addEventListener('click', () => {
+        showCountryInfo(country);
+      });
+    });
+  }
+  
+  function showCountryInfo(country) {
+    const countryInfoElement = document.getElementById('country-info');
+    console.log(country)
+    const countryInfoHTML = `
+    <div class="container-info">
+      <img src="${country.flags[0]}" alt="${country.name.common} flag">
+      <div>
+        <h2>${country.name.common}</h2>
+        <p>Capital: ${country.capital}</p>
+        <p>Población: ${country.population}</p>
+        <p>Lado de la carretera: ${country.car.side}</p>
+      </div>
+    </div>
+    <button class="close" onclick="hideCountryInfo()">Cerrar</button>
+    `;
+    countryInfoElement.innerHTML = countryInfoHTML;
+    countryInfoElement.classList.remove('hidden');
+  }
+  
+  function hideCountryInfo() {
+    const countryInfoElement = document.getElementById('country-info');
+    countryInfoElement.classList.add('hidden');
+  }
